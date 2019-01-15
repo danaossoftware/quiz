2453edae3881d17e39c3327b9f765bd72c8576c9
Dana Prakoso <danaossoftware@gmail.com>
2019-01-15 22:29:47 +0700

    commit

--- scripts/check-permission.php
+++ scripts/check-permission.php
@@ -6,8 +6,8 @@ $courseId = $_GET["course-id"];
 $userId = $_SESSION["dnquiz_user_id"];
 $results = $c->query("SELECT * FROM permissions WHERE user_id='" . $userId . "' AND chapter_id='" . $chapterId . "' AND course_id='" . $courseId . "'");
 if ($results && $results->num_rows > 0) {
-    $c->query("INSERT INTO permissions (id, course_id, chapter_id, user_id, granted) VALUES ('" . uniqid() . "', '" . $courseId . "', '" . $chapterId . "', '" . $userId . "', 0)");
     echo json_encode($results->fetch_assoc());
 } else {
+    $c->query("INSERT INTO permissions (id, course_id, chapter_id, user_id, granted) VALUES ('" . uniqid() . "', '" . $courseId . "', '" . $chapterId . "', '" . $userId . "', 0)");
     echo -1;
 }
