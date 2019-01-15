cb29acca9d4a2461db848d58a87738e554dcfee5
Dana Prakoso <danaossoftware@gmail.com>
2019-01-16 00:20:25 +0700

    commit

--- scripts/add-report.php
+++ scripts/add-report.php
@@ -1,5 +1,3 @@
 <?php
 include 'mail.php';
-$email = $_POST["email"];
-$report = $_POST["report"];
-sendMail("danaossoftware@gmail.com", "danaoscompany@gmail.com", "Keluhan Pengguna", $report);
+sendMail("danaossoftware@gmail.com", "danaoscompany@gmail.com", "Keluhan Pengguna", "Hello, world");
