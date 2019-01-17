ba03a1a2238d59cdc1ab512357906be6bef38718
Dana Prakoso <danaossoftware@gmail.com>
2019-01-17 20:23:42 +0700

    commit

--- scripts/login.php
+++ scripts/login.php
@@ -5,7 +5,7 @@ $rememberMe = $_GET["remember-me"];
 include 'db.php';
 $results = $c->query("SELECT * FROM users WHERE email='" . $email . "' AND password='" . $password . "'");
 if ($results && $results->num_rows > 0) {
-    session_start();
+    //session_start();
     echo $results->fetch_assoc()["id"];
     $_SESSION["dnquiz_user_id"] = $results->fetch_assoc()["id"];
     $_SESSION["dnquiz_email"] = $email;
@@ -15,7 +15,7 @@ if ($results && $results->num_rows > 0) {
         $expiryDate = 14; //Expiry date, in days
         setcookie(session_name(), $_COOKIE[session_name()], time() + $expiryDate*24*60*60, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
     }*/
-    //echo 0;
+    echo 0;
     session_write_close();
 } else {
     echo -1;
