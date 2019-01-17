63334b69b7464c11d4d701e7daa61e2a725332fa
Dana Prakoso <danaossoftware@gmail.com>
2019-01-18 01:10:44 +0700

    commit

--- scripts/login.js
+++ scripts/login.js
@@ -59,7 +59,7 @@ function login() {
         success: function(a) {
             alert(a);
             return;
-            var response = parseInt(a);
+            /*var response = parseInt(a);
             if (response == 0) {
                 window.location.href = "home.html";
             } else {
@@ -67,7 +67,7 @@ function login() {
                 $("#error").css("display", "block");
                 $(".login-form").css("height", "360px");
                 $("#login").css("margin-top", "10px");
-            }
+            }*/
         },
         error: function(a, b, c) {
 
