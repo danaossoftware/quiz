e7ddc836d64596b0f60a0405e18f83fe969381aa
Dana Prakoso <danaossoftware@gmail.com>
2019-01-17 20:09:34 +0700

    commit

--- scripts/check-session.php
+++ scripts/check-session.php
@@ -1,12 +1,2 @@
 <?php
 alert("Hello");
-return;
-session_start();
-if (isset($_SESSION["dnquiz_user_id"]) && $_SESSION["dnquiz_user_id"] != "") {
-    session_write_close();
-    echo 0;
-} else {
-    session_write_close();
-    echo -1;
-}
-?>
