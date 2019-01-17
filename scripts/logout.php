c502784c27222c22c46c43dda363e33b11861e21
Dana Prakoso <danaossoftware@gmail.com>
2019-01-17 20:15:10 +0700

    commit

--- scripts/logout.php
+++ scripts/logout.php
@@ -1,10 +1,5 @@
 <?php
-try {
-    //session_start();
-} catch (Exception $e) {
-    echo $e->getMessage();
-    return;
-}
+session_start();
 unset($_SESSION["dnquiz_user_id"]);
 unset($_SESSION["dnquiz_email"]);
 unset($_SESSION["dnquiz_password"]);
