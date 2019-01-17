f28366f6b6eb9bf4016c2db95a61336dea97dfb3
Dana Prakoso <danaossoftware@gmail.com>
2019-01-17 15:33:59 +0700

    commit

--- scripts/get-user.php
+++ scripts/get-user.php
@@ -2,6 +2,8 @@
 session_start();
 $userId = $_SESSION["dnquiz_user_id"];
 echo $userId;
+session_write_close();
+return;
 $results = $c->query("SELECT * FROM users WHERE id='" . $userId . "'");
 if ($results && $results->num_rows > 0) {
     $row = $results->fetch_assoc();
