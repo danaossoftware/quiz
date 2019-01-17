8ba435dccc7d1dafcdf7ed946773fcfba6f2a33f
Dana Prakoso <danaossoftware@gmail.com>
2019-01-18 01:16:11 +0700

    commit

--- scripts/login.php
+++ scripts/login.php
@@ -10,11 +10,11 @@ if ($results && $results->num_rows > 0) {
     $_SESSION["userid"] = $results->fetch_assoc()["id"];
     $_SESSION["email"] = $email;
     $_SESSION["password"] = $password;
-    /*if ($rememberMe) {
+    if ($rememberMe) {
         $params = session_get_cookie_params();
         $expiryDate = 14; //Expiry date, in days
         setcookie(session_name(), $_COOKIE[session_name()], time() + $expiryDate*24*60*60, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
-    }*/
+    }
     echo 0;
 } else {
     echo -1;
