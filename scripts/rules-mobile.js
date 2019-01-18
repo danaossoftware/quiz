92eef0de22462b4ea9ca424fe7ae7ef742043af2
Dana Prakoso <danaossoftware@gmail.com>
2019-01-18 15:52:00 +0700

    commit

--- scripts/rules-mobile.js
+++ scripts/rules-mobile.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     params = params.substr(1, params.length);
     var courseId = params.split("&")[0].split("=")[1];
     var chapterId = params.split("&")[1].split("=")[1];
-    alert("Hello, world");
     $.ajax({
         type: 'GET',
         url: PHP_PATH+'get-settings.php',
@@ -22,6 +21,33 @@ $(document).ready(function() {
         }
     });
     $("#start-exam").on("click", function() {
-        window.location.href = "http://ilatih.com/quiz/quiz-mobile.html?course_id="+courseId+"&chapter_id="+chapterId;
+        $("#access-code").val("");
+        $("#enter-code-container").css("display", "flex");
+        $("#enter-code-cancel").on("click", function() {
+            $("#enter-code-container").css("display", "none");
+        });
+        $("#enter-code-ok").on("click", function() {
+            var accessCode = $("#access-code").val();
+            if (accessCode == '') {
+                return;
+            }
+            $.ajax({
+                type: 'GET',
+                url: PHP_PATH+'check-access-code.php',
+                data: {'chapter-id': chapterId, 'course-id': courseId, 'access-code': accessCode},
+                dataType: 'text',
+                cache: false,
+                success: function(a) {
+                    $("#enter-code-container").css("display", "none");
+                    if (a < 0) {
+                        // Not granted
+                        $('#prompt-text').html("Kode yang Anda masukkan salah. Silahkan hubungi pembimbing yang bersangkutan untuk menanyakan kodenya.");
+                        $("#prompt").css("display", "flex");
+                    } else {
+                        window.location.href = "quiz.html?course_id="+courseId+"&chapter_id="+chapterId;
+                    }
+                }
+            });
+        });
     });
 });
